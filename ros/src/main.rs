780ff8ad37874a092c5f655986a41ff1d2e34ead
Givko <milevjivko@gmail.com>
2026-04-16 00:59:06 +0300

    first part of the vga driver tutorial

--- ros/src/main.rs
+++ ros/src/main.rs
@@ -2,19 +2,14 @@
 #![no_main] // dont use rust's main function
 
 use core::panic::PanicInfo;
+mod vga_buffer;
 static HELLO: &[u8] = b"Hello, World!";
 
 
 #[unsafe(no_mangle)] //dont mange the name of the function
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8; 
+    vga_buffer::print_something();
 
-    for(i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte; // write the byte to the VGA buffer
-            *vga_buffer.offset(i as isize * 2 + 1) = 0x0f; // set the color to white on black
-        }
-    }
     loop {}
 }
 
