ec0fbbacc2fca19cd04354574124045636456a88
Givko <milevjivko@gmail.com>
2026-04-27 21:35:54 +0300

    add test support

--- ros/tests/basic_boot.rs
+++ ros/tests/basic_boot.rs
@@ -7,26 +7,20 @@
 use core::panic::PanicInfo;
 use ros::println;
 
-#[unsafe(no_mangle)]
+#[unsafe(no_mangle)] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-/// This function is called on panic.
-#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
+   ros::test_panic_handler(info)
 }
 
-#[cfg(test)]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    ros::test_panic_handler(info)
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
+
