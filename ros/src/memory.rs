6b90da122617ce34d954d0c2989b5c13f63520da
z.milev <z.milev@draftkings.com>
2026-05-11 21:32:08 +0300

    paging

--- ros/src/memory.rs
+++ ros/src/memory.rs
@@ -4,8 +4,36 @@ use x86_64::{
     structures::paging::PageTable,
     VirtAddr,
 };
-use x86_64::PhysAddr;
+use x86_64::{
+    PhysAddr,
+    structures::paging::{Page, PhysFrame, Mapper, Size4KiB, FrameAllocator}
+};
 use x86_64::structures::paging::OffsetPageTable;
+/// A FrameAllocator that always returns `None`.
+pub struct EmptyFrameAllocator;
+
+unsafe impl FrameAllocator<Size4KiB> for EmptyFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame> {
+        None
+    }
+}
+/// Creates an example mapping for the given page to frame `0xb8000`.
+pub fn create_example_mapping(
+    page: Page,
+    mapper: &mut OffsetPageTable,
+    frame_allocator: &mut impl FrameAllocator<Size4KiB>,
+) {
+    use x86_64::structures::paging::PageTableFlags as Flags;
+
+    let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000));
+    let flags = Flags::PRESENT | Flags::WRITABLE;
+
+    let map_to_result = unsafe {
+        // FIXME: this is not safe, we do it only for testing
+        mapper.map_to(page, frame, flags, frame_allocator)
+    };
+    map_to_result.expect("map_to failed").flush();
+}
 
 /// Initialize a new OffsetPageTable.
 ///
